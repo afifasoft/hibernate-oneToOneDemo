097ba4cbc34584813eb7439bb068cd387f786625
afifasoft <abdul.pesit@gmail.com>
2020-04-13 14:55:08 +0530

    first commit

---
+++ src/main/java/com/alakesoftware/hb01onetoone/Hb01OneToOneApplication.java
@@ -0,0 +1,14 @@
+package com.alakesoftware.hb01onetoone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//211
+@SpringBootApplication
+public class Hb01OneToOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hb01OneToOneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alakesoftware/hb01onetoone/Hb01OneToOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alakesoftware.hb01onetoone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hb01OneToOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
